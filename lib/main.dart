266d23ab1d693e9a2d9d441f51102e8f2c8ed8ac
BrosephMC <jojoglick418@gmail.com>
2024-03-11 17:15:23 -0600

    tabs progress

--- lib/main.dart
+++ lib/main.dart
@@ -29,16 +29,15 @@ class _BottomNavigationBarExampleState
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
+    //trail map
+    TrailMapPage(),
+
+    //notifications page placeholder
+    BackPackPage(),
+
+    //badges page
     Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
+      'Index 2: Badges',
       style: optionStyle,
     ),
   ];
@@ -61,21 +60,95 @@ class _BottomNavigationBarExampleState
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Icon(Icons.map),
+            label: 'Trail Map',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: Icon(Icons.cases),
+            label: 'Backpack',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(Icons.radio_button_unchecked),
+            label: 'Badges',
           ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class TrailMapPage extends StatelessWidget {
+  const TrailMapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('TabBar Sample'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.book),
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+              ),
+              Tab(
+                icon: Icon(Icons.east),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("Language"),
+            ),
+            Center(
+              child: Text("People"),
+            ),
+            Center(
+              child: Text("Mission"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BackPackPage extends StatelessWidget {
+  const BackPackPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.notifications_sharp),
+              title: Text('Notification 1'),
+              subtitle: Text('This is a notification'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.notifications_sharp),
+              title: Text('Notification 2'),
+              subtitle: Text('This is a notification'),
+            ),
+          ),
+        ],
       ),
     );
   }
