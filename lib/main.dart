fe078c1db5b0446f2ab07eec2baab5d390746ad9
e4494s <e4494s@gmail.com>
2024-03-14 16:36:11 -0500

    Added dynamic task descriptions

--- lib/main.dart
+++ lib/main.dart
@@ -67,11 +67,93 @@ class LearningBackpackApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MyAppState()..setTrailMapFromList(
+<<<<<<< Updated upstream
         // [
         //   [ 'Title 1', 'Task 1.1', 'Task 1.2', 'Task 1.3', 'Task 1.4', 'Task 1.5', 'Task 1.6' ],
         //   [ 'Title 2', 'Task 2.1', 'Task 2.2', 'Task 2.3', 'Surprise, Task 2.7' ]
         // ]
         readFromFile(p.join(Directory.current.path, 'assets', 'sample.txt')),
+=======
+        [
+          [ 
+            [ 'Language' ],
+            [
+                'Complete Duolingo course',
+                'Go to this link: duolingo.com/hereIsSomeLink',
+                'Complete up to Chapter 5',
+                'Here is some more lines'
+            ],
+            [
+                'Read [this book] on the language',
+                'Here is some additional info about the book',
+                'Idk what else to say'
+            ],
+          ],
+
+          [
+            [ 'Mission' ],
+            [
+              'Attend SIM conference',
+              'Go to this bi-annual SIM conference',
+              'Here is the date: [some date]',
+              'Here is the time: [some time]',
+              'Here is the address: [some address]'
+            ],
+            [
+              'Learn how to write newsletters',
+              'Watch this video: youtube.com/blahblah'
+            ],
+            [
+              'Find a host church',
+              'Find a host church (or multiple) to support you in your mission',
+              'Here is a helpful article for how to ask for support: somelink.com/somelink'
+            ],
+            [
+              'I need filler',
+              'Here\'s some filler',
+              'Here\'s some more',
+              'And some more',
+              'Filler',
+              'Filler',
+              'Filler'
+            ]
+          ],
+
+          [
+            [ 'Lipsum' ],
+            [
+              'Lorem ipsum',
+              'Lorem ipsum dolor sit amet',
+              'Consectetur adipiscing elit',
+              'Ut imperdiet ante mi, ac luctus dui ultrices id',
+              'Duis eu faucibus orci',
+              'Sed et sagittis est, id tempus purus'
+            ],
+            [
+              'Vestibulum gravida maximus scelerisque',
+              'Nullam accumsan luctus lectus, id aliquam odio blandit vel',
+              'Donec sollicitudin ipsum eget mauris gravida ullamcorper',
+              'Aenean imperdiet purus ac nibh iaculis, sit amet pharetra elit facilisis',
+              'Etiam ornare urna diam, ut aliquet lacus interdum id',
+              'Fusce eget aliquet nisl, in ultrices nulla',
+              'Praesent vel luctus turpis, id placerat ligula',
+              'Quisque sagittis odio nec erat malesuada iaculis'
+            ],
+            [
+              'AAAAAAAAA',
+              'AAA',
+              'AAA',
+              'AAA',
+              'AAA',
+              'AAA',
+              'AAA',
+              'AAA',
+              'AAA',
+              'AAA'
+            ]
+          ]
+        ]
+>>>>>>> Stashed changes
       ),
       child: const MaterialApp(
         home: BottomNavigationBarExample(),
@@ -83,13 +165,15 @@ class LearningBackpackApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   int numSections = 0;
   List<int> subsectionsPerSection = [];
-  List< List<bool> > trailMapSelected = [ [] ];
+  List< List<int> > numDescriptionsPerSubsection = [ [] ];
+  List< List<int> > trailMapSelected = [ [] ];
   List<String> sectionTitles = [];
   List< List<String> > subsectionTitlesPerSection = [ [] ];
+  List< List< List<String> > > subsectionDescriptionsPerSubsection = [ [ [] ] ];
 
   List<PlatformFile> files = [];
 
-  void setTrailMapFromList(List< List<String> > trailMapDesc) {
+  void setTrailMapFromList(List< List< List<String> > > trailMapDesc) {
     numSections = trailMapDesc.length;
 
     subsectionsPerSection = List<int>.generate(
@@ -99,113 +183,246 @@ class MyAppState extends ChangeNotifier {
 
     sectionTitles = List<String>.generate(
       numSections,
-      (int index) => trailMapDesc[index][0]
+      (int index) => trailMapDesc[index][0][0]
     );
 
     subsectionTitlesPerSection = List< List<String> >.generate(
       numSections,
       (int outIndex) => List<String>.generate(
         subsectionsPerSection[outIndex],
-        (int inIndex) => trailMapDesc[outIndex][inIndex + 1]
+        (int inIndex) => trailMapDesc[outIndex][inIndex + 1][0]
       )
     );
 
-    initTrailMapList();
-  }
-
-  void initTrailMapList() {
-    trailMapSelected = List< List<bool> >.generate(
+    numDescriptionsPerSubsection = List< List<int> >.generate(
       numSections,
-      (int index) => List<bool>.generate(subsectionsPerSection[index], (int index) => false)
+      (int sectionIndex) => List<int>.generate(
+        subsectionsPerSection[sectionIndex],
+        (int subsectionIndex) => trailMapDesc[sectionIndex][subsectionIndex + 1].length - 1
+      )
+    );
+
+    subsectionDescriptionsPerSubsection = List< List< List<String> > >.generate(
+      numSections,
+      (int sectionIndex) => List< List<String> >.generate(
+        subsectionsPerSection[sectionIndex],
+        (int subsectionIndex) => List<String>.generate(
+          numDescriptionsPerSubsection[sectionIndex][subsectionIndex],
+          (int subsectionDescIndex) => trailMapDesc[sectionIndex][subsectionIndex + 1][subsectionDescIndex + 1]
+        )
+      )
+    );
+
+    trailMapSelected = List< List<int> >.generate(
+      numSections,
+      (int index) => List<int>.generate(subsectionsPerSection[index], (int index) => 0)
     );
 
     notifyListeners();
   }
 }
 
-class TrailMapSection extends StatefulWidget {
+class TrailMapSection extends StatelessWidget {
   final String title;
   final List<TrailMapSubsection> children;
-  final int trailMapIndex;
 
   const TrailMapSection({
     super.key,
     required this.title,
-    required this.children,
-    required this.trailMapIndex
+    required this.children
   });
 
   @override
-  State<TrailMapSection> createState() => _TrailMapSectionState();
-}
-
-class _TrailMapSectionState extends State<TrailMapSection> {
-  @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    final int numItems = widget.children.length;
-    List<bool> selected = appState.trailMapSelected[widget.trailMapIndex];//List<bool>.generate(numItems, (int index) => false);
-
-    return SingleChildScrollView(
-      child: DataTable(
-        onSelectAll: (bool? value) {},
-
-        columns: <DataColumn>[
-          DataColumn(
-            label: Text(
-              widget.title,
-              style: const TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold
-              )
-            )
-          ),
-        ],
-        rows: List<DataRow>.generate(
-          numItems,
-          (int index) => DataRow(
-            color: MaterialStateProperty.resolveWith<Color?>(
-                (Set<MaterialState> states) {
-              // All rows will have the same selected color.
-              if (states.contains(MaterialState.selected)) {
-                return Theme.of(context).colorScheme.primary.withOpacity(0.12);
-              }
-              return null; // Use default value for other states
-            }),
-            cells: <DataCell>[
-              DataCell(
-                TrailMapSubsection(title: widget.children[index].title)
-              )
-            ],
-            selected: selected[index],
-            onSelectChanged: (bool? value) {
-              setState(() {
-                selected[index] = value!;
-                appState.trailMapSelected[widget.trailMapIndex][index] = value;
-              });
-            },
-          ),
+    return ListView(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold
+                )
+              ),
+            ),
+            for (var child in children) child,
+          ]
         ),
-      ),
+      ]
     );
   }
 }
 
-class TrailMapSubsection extends StatelessWidget {
+class TrailMapSubsection extends StatefulWidget {
   final String title;
+  final List<String> descriptionList; // One element per line
+  final int trailMapIndex;
+  final int trailMapSubindex;
 
-  const TrailMapSubsection({super.key, required this.title});
+  const TrailMapSubsection({
+    super.key,
+    required this.title,
+    required this.descriptionList,
+    required this.trailMapIndex,
+    required this.trailMapSubindex
+  });
 
   @override
+  State<TrailMapSubsection> createState() => _TrailMapSubsectionState();
+}
+
+class _TrailMapSubsectionState extends State<TrailMapSubsection> {
+  @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        title,
-        style: const TextStyle(
-          fontStyle: FontStyle.italic
-        )
-      )
+    var appState = context.watch<MyAppState>();
+
+    int selected = appState.trailMapSelected[widget.trailMapIndex][widget.trailMapSubindex];
+    const List<String> messages = [ 'Not Started', 'In Progress', 'Complete' ];
+    final List<Color> colors =[ Colors.red, Colors.orange, Colors.lightGreen[700]! ];
+    const List<double> opacities = [ 0.22, 0.25, 0.32 ];
+    const List<Icon> icons = [ Icon(Icons.close_rounded), Icon(Icons.hourglass_bottom_rounded), Icon(Icons.check_rounded) ];
+
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: ExpansionTile(
+        title: Padding(
+          padding: const EdgeInsets.only(bottom: 5),
+          child: Text(
+            widget.title,
+            style: const TextStyle(
+              fontStyle: FontStyle.italic,
+              fontSize: 18
+            ),
+          ),
+        ),
+      
+        subtitle: Row(
+          children: <Widget>[
+            const SizedBox(
+              width: 15
+            ),
+      
+            for (int i = 0; i < 3; i++)
+              Tooltip(
+                message: messages[i],
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: selected == i ? colors[i].withOpacity(opacities[i]) : Colors.transparent,
+                    shape: BoxShape.circle
+                  ),
+                  child: IconButton(
+                    icon: icons[i],
+                    color: selected == i ? colors[i] : Colors.grey,
+                    onPressed: () {
+                      setState(() {
+                        selected = i;
+                        appState.trailMapSelected[widget.trailMapIndex][widget.trailMapSubindex] = i;
+                      });
+                    },
+                  )
+                )
+              ),
+          ]
+        ),
+      
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(230, 230, 230, 1)
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(60, 10, 0, 10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const Text(
+                            'Time Spent: 1.5 hours',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              fontSize: 16
+                            ),
+                          ),
+                      
+                          const SizedBox(
+                            height: 15
+                          ),
+                      
+                          const Text(
+                            'Task Description:',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline,
+                              fontSize: 16
+                            ),
+                          ),
+                      
+                          for (var desc in widget.descriptionList)
+                            Text(
+                              desc,
+                              style: const TextStyle(
+                                fontSize: 15
+                              )
+                            ),
+                        ]
+                      ),
+                    ),
+
+                    const SizedBox(width: 25),
+
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              'My Notes:',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline,
+                                fontSize: 16
+                              ),
+                            ),
+
+                            Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(width: 2, color: const Color.fromARGB(255, 34, 0, 126)),
+                                borderRadius: const BorderRadius.all(Radius.circular(15))
+                              ),
+                              child: const Padding(
+                                padding: EdgeInsets.all(8),
+                                child: TextField(
+                                  decoration: null,
+                                  keyboardType: TextInputType.multiline,
+                                  minLines: 1,
+                                  maxLines: null
+                                ),
+                              ),
+                            ),
+                          ]
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ]
+      ),
     );
   }
 }
@@ -289,13 +506,15 @@ class TrailMapPage extends StatelessWidget {
     var sectionTitles = appState.sectionTitles;
     var numSections = appState.numSections;
     var subsectionTitlesPerSection = appState.subsectionTitlesPerSection;
+    var subsectionsPerSection = appState.subsectionsPerSection;
+    var numDescriptionsPerSubsection = appState.numDescriptionsPerSubsection;
+    var subsectionDescriptionsPerSubsection = appState.subsectionDescriptionsPerSubsection;
 
     return DefaultTabController(
       initialIndex: 0,
       length: numSections,
       child: Scaffold(
         appBar: AppBar(
-          // title: const Text('TabBar Sample'),
           title: const Padding(
             padding: EdgeInsets.all(20.0),
             child: Column(
@@ -330,11 +549,18 @@ class TrailMapPage extends StatelessWidget {
             for (int i = 0; i < numSections; i++)
               Center(
                 child: TrailMapSection(
-                  trailMapIndex: i,
                   title: sectionTitles[i],
                   children: <TrailMapSubsection>[
-                    for (var task in subsectionTitlesPerSection[i])
-                      TrailMapSubsection( title: task ),
+                    for (int j = 0; j < subsectionsPerSection[i]; j++)
+                      TrailMapSubsection(
+                        title: subsectionTitlesPerSection[i][j],
+                        descriptionList: <String>[
+                          for (int k = 0; k < numDescriptionsPerSubsection[i][j]; k++)
+                            subsectionDescriptionsPerSubsection[i][j][k],
+                        ],
+                        trailMapIndex: i,
+                        trailMapSubindex: j,
+                      ),
                   ]
                 )
               ),
@@ -385,7 +611,7 @@ class JourneyPage extends StatelessWidget {
             for (int index = 1; index < 21; index++)
               ListTile(
                 onTap: () {
-                  print("$index was pressed!");
+                  //print("$index was pressed!");
                 },
                 leading: ExcludeSemantics(
                   child: CircleAvatar(child: Text('$index')),
